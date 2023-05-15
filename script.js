b938bf57aa11b9c61d713a9f1f74a33ff3b72047
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-15 23:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+//your code here
+let priceList=document.querySelectorAll(".price")
+let table = document.querySelector("table");
+function func(){
+	let sum=0;
+	priceList.forEach((e)=>{
+		sum += +e.innerText
+	})
+	let tr= document.createElement("tr");
+    let td1=document.createElement("td");
+    td1.innerText="Total";
+	let td2=document.createElement("td");
+    td2.innerText=sum;
+	//td2.data-ns-test="grandTotal"
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	table.appendChild(tr);
+
+}
+
+func()
